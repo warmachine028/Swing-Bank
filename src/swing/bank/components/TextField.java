8b7c59a9b28a68e59f89ac5e88985bc81d6447ab
warmachine028 <warmachine028@outlook.com>
2023-05-03 15:40:54 +0530

    feat: added TextField Component

--- src/swing/bank/components/TextField.java
+++ src/swing/bank/components/TextField.java
@@ -1,2 +1,18 @@
-package swing.bank.Components;public class TextField {
+package swing.bank.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextField extends JTextField {
+    Font font = new Font("Raleway", Font.BOLD, 14);
+    public TextField(JFrame frame, int yAxis) {
+        super();
+        setProperties(yAxis);
+        frame.add(this);
+    }
+
+    void setProperties(int y) {
+        setFont(font);
+        setBounds(300, y, 400, 30);
+    }
 }
