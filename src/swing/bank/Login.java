9adce8893cbeb7f1a149587988dfaa5338ba08d1
warmachine028 <warmachine028@outlook.com>
2023-05-03 15:42:33 +0530

    feat: used reusable components to reduce code

--- src/swing/bank/Login.java
+++ src/swing/bank/Login.java
@@ -1,7 +1,10 @@
 package swing.bank;
 
+import swing.bank.components.Button;
+import swing.bank.components.Labels.FieldLabel;
+import swing.bank.components.Labels.TitleLabel;
+
 import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,7 +12,8 @@ import java.awt.event.ActionListener;
 public class Login extends JFrame implements ActionListener {
     JTextField cardNoTextField;
     JPasswordField pinTextField;
-    JButton logInButton, clearButton, signUpButton;
+    Button logInButton, clearButton, signUpButton;
+
     Login() {
         /* Setting Properties of Login Window */
         setProperties();
@@ -19,66 +23,38 @@ public class Login extends JFrame implements ActionListener {
                 scaledIcon = new ImageIcon(
                         icon.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)
                 );
-        JLabel iconLabel = new JLabel(scaledIcon),
-                title = new JLabel("Welcome to Swing Bank"),
-                cardNoLabel = new JLabel("Card No: "),
-                pinLabel = new JLabel("PIN: ");
+        JLabel iconLabel = new JLabel(scaledIcon);
+        new TitleLabel("Welcome to Swing Bank", 200, 40, Color.GREEN, this);
+        new FieldLabel("Card No.: ", 120, 150, 150, 30, this);
+        new FieldLabel("PIN: ", 120, 220, 250, 30, this);
+
         cardNoTextField = new JTextField();
         pinTextField = new JPasswordField();
-        logInButton = new JButton("LOG IN");
-        clearButton = new JButton("CLEAR");
-        signUpButton = new JButton("SIGN UP");
+
+        logInButton = new Button("LOG IN", 300, 300, 100, this);
+        signUpButton = new Button("SIGN UP", 430, 300, 100, this);
+        clearButton = new Button("CLEAR", 300, 350, 230, this);
 
         /* Setting Properties and Location of components */
         iconLabel.setBounds(70, 10, 100, 100);
 
-        title.setFont(new Font("Oswald", Font.BOLD, 38));
-        title.setForeground(ColorUIResource.GREEN);
-        title.setBounds(200, 40, 400, 50);
-
-        cardNoLabel.setFont(new Font("Raleway", Font.BOLD, 28));
-        cardNoLabel.setForeground(ColorUIResource.GREEN);
-        cardNoLabel.setBounds(120, 150, 150, 30);
-
         cardNoTextField.setFont(new Font("Arial", Font.BOLD, 14));
         cardNoTextField.setBounds(300, 150, 230, 30);
 
-        pinLabel.setFont(new Font("Raleway", Font.BOLD, 28));
-        pinLabel.setForeground(ColorUIResource.GREEN);
-        pinLabel.setBounds(120, 220, 250, 30);
-
         pinTextField.setFont(new Font("Arial", Font.BOLD, 14));
         pinTextField.setBounds(300, 220, 230, 30);
-
-        logInButton.setBackground(Color.BLACK);
-        logInButton.setForeground(Color.WHITE);
-        logInButton.setBounds(300, 300, 100, 30);
-
-        clearButton.setBackground(Color.BLACK);
-        clearButton.setForeground(Color.WHITE);
-        clearButton.setBounds(430, 300, 100, 30);
-
-        signUpButton.setBackground(Color.BLACK);
-        signUpButton.setForeground(Color.WHITE);
-        signUpButton.setBounds(300, 350, 230, 30);
-
 
         /* Adding Event Listeners */
         logInButton.addActionListener(this);
         clearButton.addActionListener(this);
         signUpButton.addActionListener(this);
 
-        setVisible(true);
         /* Adding Components to frame */
         add(iconLabel);
-        add(title);
-        add(cardNoLabel);
-        add(pinLabel);
         add(cardNoTextField);
         add(pinTextField);
-        add(logInButton);
-        add(clearButton);
-        add(signUpButton);
+
+        setVisible(true);
     }
 
     public static void main(String[] args) {
