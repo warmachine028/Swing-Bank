a480a5a321a5d0840ef6a17c339014bd1411c87d
warmachine028 <warmachine028@outlook.com>
2023-06-06 00:26:34 +0530

    add: added createAdditionalDetailsTable function in Connector.java
    refactor: renamed createTable function to createSignUpTable in Connector.java

--- src/swing/bank/Connector.java
+++ src/swing/bank/Connector.java
@@ -26,7 +26,8 @@ public class Connector {
             statement = connection.createStatement();
             execute("CREATE DATABASE IF NOT EXISTS SwingBank;");
             execute("USE SwingBank;");
-            createTable();
+            createSignUpTable();
+            createAdditionalDetailsTable();
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
@@ -34,8 +35,7 @@ public class Connector {
 
     static long generateFormNumber() {
         String query = "SELECT IFNULL(MAX(formNo)+1, 1) AS NextFormNumber FROM Users;";
-        Connector connector = new Connector();
-        Statement statement = connector.statement;
+        Statement statement = new Connector().statement;
         try (ResultSet result = statement.executeQuery(query)) {
             result.next();
             return result.getLong("NextFormNumber");
@@ -45,7 +45,7 @@ public class Connector {
         return -1;
     }
 
-    void createTable() {
+    void createSignUpTable() {
         String query = "CREATE TABLE IF NOT EXISTS Users (" +
                 "            formNo INT AUTO_INCREMENT PRIMARY KEY," +
                 "            name VARCHAR(255) NOT NULL," +
@@ -58,6 +58,25 @@ public class Connector {
                 "            city VARCHAR(100) NOT NULL," +
                 "            state VARCHAR(100) NOT NULL," +
                 "            pinCode VARCHAR(20) NOT NULL" +
+                "        );";
+        execute(query);
+    }
+
+    void createAdditionalDetailsTable() {
+        String query = "CREATE TABLE IF NOT EXISTS AdditionalDetails (" +
+                "            id INT AUTO_INCREMENT PRIMARY KEY," +
+                "            formNo INT NOT NULL," +
+                "            religion ENUM('Hindu', 'Muslim', 'Sikh', 'Other') NOT NULL," +
+                "            category ENUM('General', 'OBC', 'SC', 'ST', 'Other') NOT NULL," +
+                "            income ENUM('No Fixed Income', '< 1,50,000', '< 2,50,000', '< 5,00,000', '< 10,00,000', 'More than 10,00,000') NOT NULL," +
+                "            qualification ENUM('Non Graduate', 'Graduate', 'Post Graduate', 'Doctorate', 'Other') NOT NULL," +
+                "            occupation ENUM('Student', 'Salaried', 'Self Employed', 'Business', 'Retired', 'Other') NOT NULL," +
+                "            pan CHAR(10) NOT NULL," +
+                "            aadhaar CHAR(12) NOT NULL," +
+                "            existingHolder ENUM('Yes', 'No') NOT NULL," +
+                "            seniorCitizen ENUM('Yes', 'No') NOT NULL," +
+                "            UNIQUE KEY (formNo)," +
+                "            FOREIGN KEY (formNo) REFERENCES Users(formNo)" +
                 "        );";
         execute(query);
     }
