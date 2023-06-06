0a3cc2b67d793c38d611956f9bd547916fb67194
warmachine028 <warmachine028@outlook.com>
2023-06-06 22:48:02 +0530

    add: added setLabels, setFields in Login Frame

--- src/swing/bank/Login.java
+++ src/swing/bank/Login.java
@@ -6,34 +6,49 @@ import swing.bank.components.Labels.TitleLabel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.Objects;
 
 public class Login extends JFrame {
     JTextField cardNoTextField;
     JPasswordField pinTextField;
-    Button logInButton, clearButton, signUpButton;
 
     Login() {
         /* Setting Properties of Login Window */
         setProperties();
+        setLabels();
+        setFields();
+        setVisible(true);
+    }
 
-        /* Creating Components */
-        ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("icons/logo.png"))),
-                scaledIcon = new ImageIcon(icon.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+    public static void main(String[] args) {
+        new Login();
+    }
+
+    private void setLabels() {
+        ImageIcon icon = new ImageIcon(
+                Objects.requireNonNull(
+                        getClass().getClassLoader()
+                                .getResource("icons/logo.png")
+                )
+        ), scaledIcon = new ImageIcon(
+                icon.getImage()
+                        .getScaledInstance(100, 100, Image.SCALE_DEFAULT)
+        );
         JLabel iconLabel = new JLabel(scaledIcon);
+        iconLabel.setBounds(70, 10, 100, 100);
+        add(iconLabel);
+
         new TitleLabel("Welcome to Swing Bank", 200, 40, Color.GREEN, this);
         new FieldLabel("Card No.: ", 120, 150, 150, 30, this);
         new FieldLabel("PIN: ", 120, 220, 250, 30, this);
+    }
 
+    private void setFields() {
         cardNoTextField = new JTextField();
         pinTextField = new JPasswordField();
 
-        logInButton = new Button("LOG IN", 300, 300, 100, this);
-        signUpButton = new Button("SIGN UP", 430, 300, 100, this);
-        clearButton = new Button("CLEAR", 300, 350, 230, this);
-
         /* Setting Properties and Location of components */
-        iconLabel.setBounds(70, 10, 100, 100);
 
         cardNoTextField.setFont(new Font("Arial", Font.BOLD, 14));
         cardNoTextField.setBounds(300, 150, 230, 30);
@@ -42,33 +57,34 @@ public class Login extends JFrame {
         pinTextField.setBounds(300, 220, 230, 30);
 
         /* Adding Event Listeners */
-        logInButton.addActionListener((event) -> System.out.println("Login"));
-        clearButton.addActionListener((event) -> {
-            cardNoTextField.setText("");
-            pinTextField.setText("");
-        });
-        signUpButton.addActionListener((event) -> {
-            setVisible(false);
-            new SignUp(this);
-        });
+        new Button("LOG IN", 300, 300, 100, this).addActionListener((event) -> System.out.println("Login"));
+        new Button("SIGN UP", 430, 300, 100, this).addActionListener(this::handleSignUp);
+        new Button("CLEAR", 300, 350, 230, this).addActionListener(this::handleClear);
 
         /* Adding Components to frame */
-        add(iconLabel);
         add(cardNoTextField);
         add(pinTextField);
-
-        setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Login();
+    void handleClear(ActionEvent event) {
+        cardNoTextField.setText("");
+        pinTextField.setText("");
     }
 
+    void handleSignUp(ActionEvent event) {
+        setVisible(false);
+        new SignUp(this);
+    }
 
     private void setProperties() {
         setLayout(null);
-        setIconImage(Toolkit.getDefaultToolkit().getImage(
-                getClass().getClassLoader().getResource("icons/white icon.png")));
+        setIconImage(
+                Toolkit.getDefaultToolkit()
+                        .getImage(
+                                getClass().getClassLoader()
+                                        .getResource("icons/white icon.png")
+                        )
+        );
         setSize(800, 500);
         getContentPane().setBackground(Color.BLACK);
         setLocation(350, 200);
