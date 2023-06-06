46c850f1dcfeafed708726354e6bd9c4eb8182d2
warmachine028 <warmachine028@outlook.com>
2023-06-06 22:52:27 +0530

    feat: added setSeniorSitizen method in AdditionalInformation Frame
    fix: added OptionPane to display Errors

--- src/swing/bank/AdditionalInformation.java
+++ src/swing/bank/AdditionalInformation.java
@@ -11,12 +11,14 @@ import swing.bank.components.TextField;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.Enumeration;
 
 public class AdditionalInformation extends JFrame {
-    String signUpFormNumber;
+    String signUpFormNumber, seniorCitizenship;
     TextField panTextField, aadhaarTextField;
     ComboBox<String> religion, category, income, qualification, occupation;
     ButtonGroup existingHolder;
@@ -32,8 +34,7 @@ public class AdditionalInformation extends JFrame {
     }
 
     public static void main(String[] args) {
-        new Connector();
-        new AdditionalInformation(null, null);
+        new AdditionalInformation(null, "1");
     }
 
     private void setLabels() {
@@ -47,6 +48,7 @@ public class AdditionalInformation extends JFrame {
         new FieldLabel("PAN Number: ", 50, 390, 300, 30, 20, this);
         new FieldLabel("Aadhaar Number: ", 50, 440, 300, 30, 20, this);
         new FieldLabel("Existing account Holder ?", 50, 490, 300, 30, 20, this);
+        new FieldLabel("Senior Citizen ?", 50, 540, 300, 30, 20, this);
     }
 
     private void setFields() {
@@ -67,6 +69,12 @@ public class AdditionalInformation extends JFrame {
                 new RadioButton("Yes", 470, 490, this),
                 new RadioButton("No", 620, 490, this)
         );
+        var seniorCitizenStatus = new ButtonGroup(
+                true,
+                new RadioButton("Yes", 470, 540, this),
+                new RadioButton("No", 620, 540, this)
+        );
+        setSeniorCitizen(seniorCitizenStatus);
 
         Button back = new Button("BACK", 380, 660, 80, this);
         back.setBackground(Color.WHITE);
@@ -80,6 +88,45 @@ public class AdditionalInformation extends JFrame {
         next.setBackground(Color.WHITE);
         next.setForeground(Color.BLACK);
         next.addActionListener(this::handleSubmit);
+    }
+
+    private void setSeniorCitizen(ButtonGroup radioButtons) {
+        Connector connection = new Connector();
+        String query = String.format("SELECT dob FROM Users WHERE formNo = '%s'", signUpFormNumber);
+        try (ResultSet result = connection.statement.executeQuery(query)) {
+            if (result.next()) {
+                Date dateOfBirth = result.getDate("dob");
+                // Calculate age based on date of birth
+                Calendar now = Calendar.getInstance(),
+                        dob = Calendar.getInstance();
+                dob.setTime(dateOfBirth);
+                int age = now.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+                if (now.get(Calendar.MONTH) < dob.get(Calendar.MONTH) ||
+                        (now.get(Calendar.MONTH) == dob.get(Calendar.MONTH) &&
+                                now.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)))
+                    age--;
+
+                // Determine if the person is a senior citizen
+                seniorCitizenship = age >= 60 ? "Yes": "No";
+            } else {
+                throw new IllegalArgumentException("Invalid Initial User Data");
+            }
+        } catch (IllegalArgumentException | SQLException exception) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    exception.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+        Enumeration<AbstractButton> buttons = radioButtons.getElements();
+        while (buttons.hasMoreElements()) {
+            AbstractButton button = buttons.nextElement();
+            if (button.getText().equals(seniorCitizenship)) {
+                button.setSelected(true);
+                break;
+            }
+        }
     }
 
     void handleBack(ActionEvent event) {
@@ -108,14 +155,9 @@ public class AdditionalInformation extends JFrame {
                 occupation = (String) this.occupation.getSelectedItem(),
                 pan = panTextField.getText(),
                 aadhaar = aadhaarTextField.getText(),
-                existingHolder,
-                seniorCitizenship;
+                existingHolder;
         RadioButton existingHolderStatus = getSelection(this.existingHolder);
         try {
-            Connector connection = new Connector();
-            String query = String.format("SELECT dob FROM Users WHERE formNo = '%s'", signUpFormNumber);
-            ResultSet result = connection.statement.executeQuery(query);
-            result.next();
             Validator.validateSelection(religion, "a religion");
             Validator.validateSelection(category, "a category");
             Validator.validateSelection(income, "an income");
@@ -124,10 +166,10 @@ public class AdditionalInformation extends JFrame {
             Validator.validatePan(pan);
             Validator.validateAadhaar(aadhaar);
             Validator.validateOptions(existingHolderStatus, "existing account holder status");
-            seniorCitizenship = Validator.validateSeniorCitizen(result.getDate("dob"));
             existingHolder = existingHolderStatus.getText();
 
-            query = String.format(
+            Connector connection = new Connector();
+            String query = String.format(
                     "INSERT INTO AdditionalDetails (formNo, religion, category, income, qualification," +
                             " occupation, pan, aadhaar, existingHolder, seniorCitizen) " +
                             "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
@@ -135,7 +177,6 @@ public class AdditionalInformation extends JFrame {
                     existingHolder, seniorCitizenship
             );
             connection.statement.executeUpdate(query);
-
         } catch (NullPointerException | IllegalArgumentException exception) {
             JOptionPane.showMessageDialog(
                     this,
@@ -143,7 +184,11 @@ public class AdditionalInformation extends JFrame {
                     "Invalid Input",
                     JOptionPane.WARNING_MESSAGE);
         } catch (SQLException exception) {
-            System.out.println(exception.getCause() + exception.getMessage());
+            JOptionPane.showMessageDialog(
+                    this,
+                    exception.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
         }
     }
 
