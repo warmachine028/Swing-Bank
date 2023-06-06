75417804abe4df097fc1f1e7c34da3fcfc32d907
warmachine028 <warmachine028@outlook.com>
2023-06-06 22:45:32 +0530

    feat: added boolean Constructor to disable buttons in Buttons/ButtonGroup

--- src/swing/bank/components/Buttons/ButtonGroup.java
+++ src/swing/bank/components/Buttons/ButtonGroup.java
@@ -1,8 +1,25 @@
 package swing.bank.components.Buttons;
 
+import javax.swing.*;
+import java.util.Enumeration;
+
 public class ButtonGroup extends javax.swing.ButtonGroup {
+
+    public ButtonGroup(boolean disabled, RadioButton... buttons) {
+        this(buttons);
+        disable();
+    }
+
     public ButtonGroup(RadioButton... buttons) {
         for (RadioButton button : buttons)
             add(button);
+    }
+
+    void disable() {
+        Enumeration<AbstractButton> buttons = this.getElements();
+        while (buttons.hasMoreElements()) {
+            AbstractButton button = buttons.nextElement();
+            button.setEnabled(false);
+        }
     }
 }
